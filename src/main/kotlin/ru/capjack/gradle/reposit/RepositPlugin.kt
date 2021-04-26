177c537ad2b3cf17b36e69e8834435a12a043d6d
Alexey Chuprin <a.chuprin@capjack.ru>
2021-04-26 11:41:44 +0300

    Add publishOnNebulaRelease

--- src/main/kotlin/ru/capjack/gradle/reposit/RepositPlugin.kt
+++ src/main/kotlin/ru/capjack/gradle/reposit/RepositPlugin.kt
@@ -3,10 +3,22 @@ package ru.capjack.gradle.reposit
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByType
 
 class RepositPlugin : Plugin<Project> {
 	override fun apply(project: Project) {
 		project.extensions.create<RepositExtension>(EXCEPTION, project)
+		
+		project.afterEvaluate {
+			val ext = extensions.getByType<RepositExtension>()
+			if (ext.publishOnNebulaRelease
+				&& pluginManager.hasPlugin("org.gradle.maven-publish")
+				&& pluginManager.hasPlugin("nebula.release")
+			) {
+				tasks["postRelease"].finalizedBy("publish")
+			}
+		}
 	}
 	
 	companion object {
