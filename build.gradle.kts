c578c87ebb21d6358b2105f6d7a723625b6be4d4
Alexey Chuprin <a.chuprin@capjack.ru>
2021-06-23 10:44:26 +0300

    Kotlin 1.5.10 Gradle 7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	`kotlin-dsl`
 	`java-gradle-plugin`
 	`maven-publish`
-	id("com.gradle.plugin-publish") version "0.14.0"
+	id("com.gradle.plugin-publish") version "0.15.0"
 	id("nebula.release") version "15.3.1"
 }
 
